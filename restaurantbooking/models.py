ad04deb82ec154402858212da505360a20de6a0d
NicoleHitter <nicole.hitter92@gmail.com>
2023-04-16 12:48:04 +0000

    Creating Reservation Model

--- restaurantbooking/models.py
+++ restaurantbooking/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
+
+GROUPSIZE_CHOICES = (
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "4"),
+    ("5", "5"),
+    ("6", "6"),
+    ("7", "7"),
+    ("8", "8"),
+    )
+
+TIME_CHOICES = (
+    ("12 PM", "12 PM"),
+    ("12:30 PM", "12:30 PM"),
+    ("1 PM", "1 PM"),
+    ("1:30 PM", "1:30 PM"),
+    ("2 PM", "2 PM"),
+    ("2:30 PM", "2:30 PM"),
+    ("3 PM", "3 PM"),
+    ("3:30 PM", "3:30 PM"),
+    ("4 PM", "4 PM"),
+    ("4:30 PM", "4:30 PM"),
+    ("5 PM", "5 PM"),
+    ("5:30 PM", "5:30 PM"),
+    ("6 PM", "6 PM"),
+    ("6:30 PM", "6:30 PM"),
+    ("7 PM", "7 PM"),
+    ("7:30 PM", "7:30 PM"),
+    ("8 PM", "8 PM"),
+    ("8:30 PM", "8:30 PM"),
+)
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    groupsize = models.CharField(max_length=50, choices=GROUPSIZE_CHOICES, default="Group Size")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(max_length=18, choices=TIME_CHOICES, default="12 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
