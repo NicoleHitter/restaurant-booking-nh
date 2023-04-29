c4170fd12bb135a3425d7ae3c3471f7e99346db0
NicoleHitter <nicole.hitter92@gmail.com>
2023-04-29 22:13:52 +0000

    Add body style and color

--- booking/settings.py
+++ booking/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
 import dj_database_url
 from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
@@ -33,7 +32,8 @@ CLOUDINARY_STORAGE = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+DEVELOPMENT = True
 
 ALLOWED_HOSTS = ['restaurant-booking-nh.herokuapp.com', 'localhost']
 
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-      'django.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -151,7 +151,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
